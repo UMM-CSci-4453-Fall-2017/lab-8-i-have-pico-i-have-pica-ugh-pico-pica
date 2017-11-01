1002939e22f59bff57921506be6c3aaa7ecebaec
Xaitheng Yang <yang3792@reliant.morris.umn.edu>
2017-11-01 16:36:38 -0500

    work on SQL queries and future planning for button functionality

--- express.js
+++ express.js
@@ -1,3 +1,5 @@
+async = require("async");
+
 var express=require('express'),
 mysql=require('mysql'),
 credentials=require('./credentials.json'),
@@ -12,7 +14,7 @@ connection.connect(function(err){if(err){console.log(error)}});
 
 app.use(express.static(__dirname + '/public'));
 app.get("/buttons",function(req,res){
-  var sql = 'SELECT * FROM test.till_buttons';
+  var sql = 'SELECT * FROM XaiMarsh.till_buttons';
   connection.query(sql,(function(res){return function(err,rows,fields){
      if(err){console.log("We have an error:");
              console.log(err);}
@@ -20,15 +22,24 @@ app.get("/buttons",function(req,res){
   }})(res));
 });
 app.get("/click",function(req,res){
-  var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->"+sql+"<-");
+  	var id = req.param('id');
+  	var sql = 'SELECT prices from prices where id = '+id;
+  	var item_info;
 
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);}
-     res.send(err); // Let the upstream guy know how it went
-  }})(res));
+	console.log("Attempting sql ->"+sql+"<-");
+
+	async.series([
+		function(callback){
+			connection.query(sql,(function(res){return function(err,rows,fields){
+    				if(err){console.log("We have an insertion error:");
+        	     			console.log(err);}
+    	 			res.send(err); // Let the upstream guy know how it went
+				item_info = rows[0][prices];
+
+	  		}})(res));
+			
+			callback();
+		}]);
 });
 // Your other API handlers go here!
 
