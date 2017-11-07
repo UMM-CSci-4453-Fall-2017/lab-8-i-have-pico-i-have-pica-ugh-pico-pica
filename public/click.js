e323bc4ab21bd9f91c8e333605f563751cfa39a9
hoff0899 <hoff0899@morris.umn.edu>
2017-11-07 17:44:22 -0600

    Start implementation of a current transaction list, and delete function

--- public/click.js
+++ public/click.js
@@ -3,8 +3,15 @@ angular.module('buttons',[])
   .factory('buttonApi',buttonApi)
   .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
+//Helpful links
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+//https://www.w3schools.com/jsref/jsref_tofixed.asp
+
+
 function ButtonCtrl($scope,buttonApi){
+   $scope.currentPrice=0;
    $scope.buttons=[]; //Initially all was still
+   $scope.order=[];
    $scope.errorMessage='';
    $scope.isLoading=isLoading;
    $scope.refreshButtons=refreshButtons;
@@ -31,13 +38,32 @@ function ButtonCtrl($scope,buttonApi){
   function buttonClick($event){
      $scope.errorMessage='';
      buttonApi.clickButton($event.target.id)
-        .success(function(//put a variable here that will contain the result of clickButton
-	){
-  	//put what you want the button to do here
-  })
-        .error(function(){$scope.errorMessage="Unable click";});
+        .success(refreshItems)
+        .error(function(){$scope.errorMessage="Unable to click";});
+  }
+  function refreshItems(){
+    loading=true;
+    $scope.errorMessage='';
+    buttonApi.getItems()
+      .success(function(data){
+        $scope.order=data;
+        $scope.currentPrice=0;
+        data=data.map(function(item) {
+          var priceToBeAdded = (item.count * item.price).toFixed(2);
+          item.price = (item.price).toFixed(2);
+          $scope.currentPrice += priceToBeAdded;
+          return item;
+        });
+        $scope.currentPrice = ($scope.currentPrice).toFixed(2);
+        loading=false;
+      })
+      .error(function() {
+        $scope.errorMessage="Item load request failed!";
+        loading=false;
+      });
   }
   refreshButtons();  //make sure the buttons are loaded
+  refreshItems();
 
 }
 
@@ -47,10 +73,17 @@ function buttonApi($http,apiUrl){
       var url = apiUrl + '/buttons';
       return $http.get(url);
     },
+    getItems: function() {
+      var url = apiUrl + '/item='+id;
+      return $http.get(url);
+    },
     clickButton: function(id){
       var url = apiUrl+'/click?id='+id;
-//      console.log("Attempting with "+url);
-      return $http.get(url); // Easy enough to do this way
+      return $http.post(url); // Easy enough to do this way
     }
+    // deleteTransaction: function(id) {
+    //   var url = apiUrl + '/click?delete='+id;
+    //   return $http.delete(url);
+    // }
  };
 }
